7ad3aa5c6d3637f2f214c591e43abf4b02449bed
CopterDoctor <Jordananders@me.com>
2018-06-12 16:38:36 -0700

    can now add and delete from list. removed auto redirect to urlLONG

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 function generateRandomString() {
   // ****************THIS FUNCTION IS TEMPORARY***********
@@ -13,7 +13,7 @@ function generateRandomString() {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
   };
-  return base62.encode(getRandomIntInclusive(100, 1000));
+  return base62.encode(getRandomIntInclusive(10000, 100000));
 }
 
 let urlDatabase = {
@@ -21,32 +21,36 @@ let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+// Redirect to full url when shorturl entered /u/<shorturl>
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(302, `${longURL}`);
+});
 
+app.post('/u/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect(301, '/urls');
+});
+
+// Render index of urls in urlDatabse object
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
 });
-
-app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
-  console.log(longURL);
-  res.redirect(301, `${longURL}`);
+// Append long webaddress to urlDatabase with randomly generated short URL
+app.post("/urls", (req, res) => {
+  let randomString = generateRandomString();
+  urlDatabase[randomString] = req.body.longURL;
+  res.redirect(302, `/urls`);
 });
-
+// Render enter new URL page
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
-
+// Render urls_show
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id };
   res.render("urls_show", templateVars);
-});
-
-app.post("/urls", (req, res) => {
-  let randomString = generateRandomString();
-  // console.log(`app.post ${req.body.longURL}`);  // debug statement to see POST parameters
-  urlDatabase[randomString] = req.body.longURL;
-  res.redirect(302, `/u/${randomString}`);
 });
 
 app.listen(8080);
